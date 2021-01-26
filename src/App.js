da47e809fa4981891a63df9321cf16f12e917be8
Your Name <nusret-24@hotmail.co.uk>
2021-01-26 21:23:08 +0100

    first changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,52 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Recept</h1>
-        <h2>Recept body</h2>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div class="grid-container">
+				<div class="item1">
+					<div>Welcome to food & drinks!</div>
+					<div>Username84</div>
+				</div>
+				<div class="item2">
+					<div class="menuButton">Menu</div>
+					<div class="menu">
+          <div>Start Page</div>
+          <div>Start Page</div>
+          <div>Start Page</div>
+          <div>Start Page</div>
+          <div>Start Page</div>
+          <div>Start Page</div>
+          <div>Start Page</div>
+          </div>
+				</div>
+				<div class="item3">
+					<div>
+						<div class="card">1</div>
+						<div class="card">2</div>
+						<div class="card">3</div>
+						<div class="card">4</div>
+						<div class="card">1</div>
+						<div class="card">2</div>
+						<div class="card">3</div>
+						<div class="card">4</div>
+					</div>
+
+					<div>
+						<div class="card">1</div>
+						<div class="card">2</div>
+						<div class="card">3</div>
+						<div class="card">4</div>
+						<div class="card">1</div>
+						<div class="card">2</div>
+						<div class="card">3</div>
+						<div class="card">4</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
