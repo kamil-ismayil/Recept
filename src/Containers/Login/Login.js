3d63e686a98ce6fefd1fc591e6691a87c6feebd7
Your Name <nusret-24@hotmail.co.uk>
2021-02-03 00:28:17 +0100

    added drinks to json and images

--- src/Containers/Login/Login.js
+++ src/Containers/Login/Login.js
@@ -1,35 +1,44 @@
-import React, { Component, useState } from 'react';
-import Mainpage from '../Mainpage/Mainpage';
-import './Login.css';
+import React, { Component, useState } from "react";
+import Mainpage from "../Mainpage/Mainpage";
+import "./Login.css";
 
 const login = (props) => {
+	const [enteredUserName, setEnteredUserName] = useState("");
+	const [enteredPassword, setEnteredPassword] = useState("");
 
-    const [enteredUserName, setEnteredUserName] = useState('');
-    const [enteredPassword, setEnteredPassword] = useState('');
+	const loginHandler = (event) => {
+		event.preventDefault();
+	};
 
-    const loginHandler = (event) => {
-        
-        event.preventDefault();        
-    }
-    
-    return (
-        <section className="main-form">            
-            <form onSubmit={loginHandler} >                
-                <div className="login">
-                    <h2>Please login to continue..</h2>
-                    <label htmlFor="username">Username</label>
-                    <input type="text" id="username" value={enteredUserName} 
-                            onChange={event => setEnteredUserName(event.target.value)} />
-                </div>
-                <div className="login">
-                    <label htmlFor="password">Password</label>           
-                    <input type="password" id="password" value={enteredPassword} 
-                            onChange={event => setEnteredPassword(event.target.value)} />
-                    <button type="submit" >Sign in</button>        
-                </div> 
-            </form>            
-        </section>
-    )
-}
+	return (
+		<section className="main-form">
+			<form onSubmit={loginHandler}>
+				<div className="login">
+					<h2>Please login to continue..</h2>
+					<label htmlFor="username">Username</label>
+					<input
+						type="text"
+						id="username"
+						value={enteredUserName}
+						onChange={(event) => setEnteredUserName(event.target.value)}
+					/>
+				</div>
+				<div className="login">
+					<label htmlFor="password">Password</label>
+					<input
+						type="password"
+						id="password"
+						value={enteredPassword}
+						onChange={(event) => setEnteredPassword(event.target.value)}
+					/>
+					<div class="buttonContainer">
+						<button class="bRow" type="submit">Create account</button>
+						<button class="bRow" type="submit">Sign in</button>
+					</div>
+				</div>
+			</form>
+		</section>
+	);
+};
 
-export default login; 
+export default login;
